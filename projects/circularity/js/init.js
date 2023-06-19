73ab0a7ec97778259c818d5b8614a50bce75526e
Addison Dixon <addisondixon2007@icloud.com>
2023-06-19 16:40:49 +0000

    started circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,12 +20,24 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle;
+        var circles = [];
 
 
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle(){ }
         
 
         // TODO 3 / 7 : Call the drawCircle() function 
+         drawCircle();
+         drawCircle();
+         drawCircle();
+         drawCircle();
+         drawCircle();
+
+
+
+
 
 
         ////////////////////////////////////////////////////////////
@@ -39,10 +51,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            physikz.updatePosition([1])
+            physikz.updatePosition([2])
+            physikz.updatePosition([3])
+            physikz.updatePosition([4])
+            physikz.updatePosition([5])
 
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+           game.checkCirclePosition([1])
+           game.checkCirclePosition([2])
+           game.checkCirclePosition([3])
+           game.checkCirclePosition([4])
+           game.checkCirclePosition([5])
 
             // TODO 9 : Iterate over the array
            
